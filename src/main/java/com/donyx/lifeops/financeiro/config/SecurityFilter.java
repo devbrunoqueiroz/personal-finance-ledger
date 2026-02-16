1008088f02b035bc90de012b53707c873e0eb9e5
Bruno Queiroz <devbruno.queiroz@gmail.com>
2026-02-15 22:33:05 -0300

    basic security configuration

---
+++ src/main/java/com/donyx/lifeops/financeiro/config/SecurityFilter.java
@@ -0,0 +1,25 @@
+package com.donyx.lifeops.financeiro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityFilter {
+
+    @Bean
+    SecurityFilterChain security(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/actuator/health").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .build();
+    }
+}
