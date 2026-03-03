3bd8c8a76e9884237bcd728a6dd9721f9492e6d7
Bruno Queiroz <devbruno.queiroz@gmail.com>
2026-03-02 21:10:19 -0300

    Transactions exceptions

--- src/main/java/com/donyx/lifeops/financeiro/domain/common/exception/CreationDateException.java
+++
@@ -1,8 +0,0 @@
-package com.donyx.lifeops.financeiro.domain.common.exception;
-
-public class CreationDateException extends DomainException{
-
-    protected CreationDateException(String message) {
-        super(message);
-    }
-}
